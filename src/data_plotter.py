44b27284b62f4dbf7381de8f1ad4cf7f16bff5bb
Bazyli Polednia <bazyli.polednia@gmail.com>
2020-12-14 02:06:31 +0100

    Fix plot axis

--- src/data_plotter.py
+++ src/data_plotter.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import cm
 
-RESULTS_PATH = "data/experiment/experiment_results_delete_1000.txt"
+RESULTS_PATH = "data/experiment/experiment_results_update_1000.txt"
 
 with open(RESULTS_PATH, "r") as results, open("data/experiment/results.txt", "w") as output:
     for line in results.readlines():
@@ -17,7 +17,7 @@ with open("data/experiment/results.txt", "r") as results:
 
 alphas = np.arange(0.1, 1, 0.1)
 max_pages = np.arange(1, 10, 1)
-alphas, max_pages = np.meshgrid(alphas, max_pages)
+max_pages, alphas = np.meshgrid(max_pages, alphas)
 disk_operations = [probe[2] for probe in data]
 time = [probe[3] for probe in data]
 disk_operations = np.array(disk_operations).reshape(len(alphas), len(max_pages))
@@ -25,20 +25,20 @@ time = np.array(time).reshape(len(alphas), len(max_pages))
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surface = ax.plot_surface(alphas, max_pages, time, cmap=cm.coolwarm,
+surface = ax.plot_surface(max_pages, alphas, time, cmap=cm.coolwarm,
                           linewidth=0, antialiased=False)
-ax.set_xlabel('Alpha')
-ax.set_ylabel('Max overflow pages')
+ax.set_xlabel('Max overflow pages')
+ax.set_ylabel('Alpha')
 ax.set_zlabel('Time')
-plt.savefig("data/plots/plot_time_delete_1000.png")
+plt.savefig("data/plots/plot_time_update_1000.png")
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-surface = ax.plot_surface(alphas, max_pages, disk_operations, cmap=cm.coolwarm,
+surface = ax.plot_surface(max_pages, alphas, disk_operations, cmap=cm.coolwarm,
                           linewidth=0, antialiased=False)
 ax.view_init(30, 60)
-ax.set_xlabel('Alpha')
-ax.set_ylabel('Max overflow pages')
-ax.set_zlabel('Disk operations')
+ax.set_xlabel('Max overflow pages')
+ax.set_ylabel('Alpha')
+ax.set_zlabel('Time')
 
-plt.savefig("data/plots/plot_disk_delete_1000.png")
+plt.savefig("data/plots/plot_disk_update_1000.png")
